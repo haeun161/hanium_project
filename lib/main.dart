3e0104d89ef4ff734cfa9af5c58e132945c0d650
이하은 <haeun161@naver.com>
2022-07-14 20:49:45 +0900

    첫번째 화면 완성과 로그인페이지 시도

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //가로방향 설정 위하여 추가
+import 'package:history_gamification/sub/firstPage.dart';
+import 'package:history_gamification/sub/loginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,29 +13,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // 가로 왼쪽, 오른쪽 방향 고정
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
     return MaterialApp(
       title: 'History Gamification',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'History Gamification'),
-    );
+      home:Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images1/background.jpg')
+             )
+            ),
+          child: Center(
+             child: Text('History\n Gamification',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white,fontSize: 50),),
+                  )
+    ));
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -43,7 +60,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
---
+++ lib/sub/loginPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        child:Center(
+          child: Text('로그인 페이지'),
+        ),
+      ),
+    );
+  }
+}
