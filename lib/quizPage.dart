08c3add3c51dfaf4585d0bffd3cfbdbb7548c360
이하은 <haeun161@naver.com>
2022-07-26 23:41:41 +0900

    finished quizPage
    퀴즈문제를 풀기전의 퀴즈 난이도를 선택하는 페이지를 구현하였다.

--- lib/quizPage.dart
+++ lib/quizPage.dart
@@ -1,23 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:history_gamification/gamePage/storyMap.dart';
+import 'package:history_gamification/mainPage.dart';
 
 class quizPage extends StatefulWidget {
   @override
-  State<quizPage> createState() => _quizPage();
+  _quizPage createState() => _quizPage();
 }
 
 class _quizPage extends State<quizPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/images1/background.jpg'), // 배경 이미지
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images1/background.jpg'), // 배경 이미지
+            ),
+          ),
+          child: SafeArea(
+            child: Center(
+              child: Container(
+                height: 300.0,
+                width: 450.0,
+                color: Color(0xff9999FF),
+                child: ListView(
+                  children:<Widget>[
+                    Container(
+                      alignment: const Alignment(-0.9,0),
+                      child: Text('퀴즈',
+                        style: TextStyle(fontSize: 40, color: Colors.white,fontWeight: FontWeight.bold),)
+                    ),
+                    Container(
+                      alignment: const Alignment(-0.6,0),
+                      child: Text('문제의 난이도(상, 중, 하)를 선택하세요.',
+                        style: TextStyle(fontSize: 20, color: Colors.white),)
+                    ),
+
+                    SizedBox( height:50.0),
+                    Row(
+                      children: <Widget>[
+                        SizedBox( width:25.0),
+                        Container(
+                            height: 90,
+                            width: 105,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images1/star.png'),
+                              ),
+                            ),
+                            child: ListView(
+                                children: <Widget>[
+                                  SizedBox( height:30.0),
+                                  TextButton(
+                                    style: TextButton.styleFrom(
+                                      textStyle: const TextStyle(fontSize: 5),padding: EdgeInsets.all(0),
+                                    ),
+                                    onPressed: (){
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => storyMap()));
+                                    },
+                                    child: Text( '하',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize:20, color:Colors.purple, fontWeight: FontWeight.bold),
+                                  ),
+                                  )
+                                ]
+                            )
+                        ),
+                        SizedBox( width:40.0),
+                        Container(
+                            height: 90,
+                            width: 105,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images1/star.png'),
+                              ),
+                            ),
+                            child: ListView(
+                                children: <Widget>[
+                                  SizedBox( height:30.0),
+                                  TextButton(
+                                    style: TextButton.styleFrom(
+                                      textStyle: const TextStyle(fontSize: 5),padding: EdgeInsets.all(0),
+                                    ),
+                                    onPressed: (){
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => storyMap()));
+                                    },
+                                    child: Text( '중',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize:20, color:Colors.purple, fontWeight: FontWeight.bold),
+                                    ),
+                                  )
+                                ]
+                            )
+                        ),
+                        SizedBox( width:40.0),
+                        Container(
+                            height: 90,
+                            width: 105,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images1/star.png'),
+                              ),
+                            ),
+                            child: ListView(
+                                children: <Widget>[
+                                  SizedBox( height:30.0),
+                                  TextButton(
+                                    style: TextButton.styleFrom(
+                                      textStyle: const TextStyle(fontSize: 5),padding: EdgeInsets.all(0),
+                                    ),
+                                    onPressed: (){
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => storyMap()));
+                                    },
+                                    child: Text( '상',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize:20, color:Colors.purple, fontWeight: FontWeight.bold),
+                                    ),
+                                  )
+                                ]
+                            )
+                        ),
+                      ],
+                    )
+                  ]
+
+                ),
+              ),
+            )
           ),
         ),
-      )
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => mainPage()));
+        },
+        child: const Icon(Icons.arrow_circle_right),
+      ),
+
     );
   }
 }
