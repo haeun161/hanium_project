5703952ac20f614fec8541aae7e7840db17ae248
이하은 <haeun161@naver.com>
2022-08-08 01:41:15 +0900

    story1succeeded texttyping animation & working on image animation
    살수대첩 첫 장면에서
    타이핑 에니메이션 효과내는 것 성공
    + 사진들이 fade in fade out되면서 애니메이션 효과 내는 것 진행중

--- lib/story1/story1.dart
+++ lib/story1/story1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:history_gamification/gamePage/storyMap.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class story1 extends StatefulWidget {
   @override
@@ -7,7 +8,7 @@ class story1 extends StatefulWidget {
 }
 
 class _story1 extends State<story1> {
-  final ScrollController _scroller = ScrollController();
+  bool _visible = true;
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +36,50 @@ class _story1 extends State<story1> {
                       },
                     )
                 ),
-                SizedBox( height:140),
+                //SizedBox( height:165),
                 Container(
-                  height: 150,
+                  height: 165,
+                  child: AnimatedOpacity(
+                    // If the widget is visible, animate to 0.0 (invisible).
+                    // If the widget is hidden, animate to 1.0 (fully visible).
+                    opacity: _visible ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 500),
+                    // The green box must be a child of the AnimatedOpacity widget.
+                    child: Container(
+                      width: 200.0,
+                      height: 200.0,
+                      color: Colors.white,
+
+
+                  ),
+                  ),
+
+
+        ),
+
+                Container( //textbox
+                  height: 140,
                   color: Colors.black54,
+                  child: Center(
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          '역사체험의 첫번째 배경은 612년 고구려야! \n 이 곳에서 고구려와 수나라가 엄청난 전쟁을 벌이지.',
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+
+                      totalRepeatCount: 1,
+                      pause: const Duration(milliseconds: 1000),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    ),
+                  )
                 )
 
               ],
